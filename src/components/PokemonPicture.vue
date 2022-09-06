c9b7973a4ba751a9e8a61093d8a3bf1cc78b2e55
abelheredia <herediabravoabel@gmail.com>
2022-09-06 17:05:34 -0500

    primer commit

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="pokemon-container">
+    <img
+      v-if="!showPokemon"
+      class="hidden-pokemon"
+      :src="imgSrc"
+      alt="pokemon"
+    />
+    <img v-if="showPokemon" class="fade-in" :src="imgSrc" alt="pokemon" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemonId: {
+      type: Number,
+      required: true,
+    },
+    showPokemon: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    imgSrc() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
+    },
+  },
+};
+</script>
+
+<style>
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  user-select: none;
+  margin: 0 auto;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>¿Quién es este pokémon?</h1>
+    <PokemonPicture :pokemonId="1" :showPokemon="true" />
+    <PokemonOptions />
+  </div>
+</template>
+
+<script>
+import PokemonPicture from "../components/PokemonPicture.vue";
+import PokemonOptions from "../components/PokemonOptions.vue";
+
+export default {
+  components: { PokemonPicture, PokemonOptions },
+};
+</script>
